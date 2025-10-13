6e85a88bf5f5fe8dcc31f152a418718a00ad9aec
al3c5 <al3c5@pm.me>
2025-10-13 17:59:38 +0200

    network optimization script

--- bin_set_network_optimizations.sh
+++ bin_set_network_optimizations.sh
@@ -6,3 +6,12 @@ sudo sysctl -w net.core.wmem_max=$MaxExpectedPathBDP
 sudo sysctl -w net.ipv4.tcp_rmem="4096 87380 $MaxExpectedPathBDP"
 sudo sysctl -w net.ipv4.tcp_wmem="4096 16384 $MaxExpectedPathBDP"
 sudo sysctl -w net.ipv4.tcp_slow_start_after_idle=0
+
+exit 0
+# make it persistent by adding to /etc/sysctl.d/local.conf
+
+net.core.rmem_max = 8388608
+net.core.wmem_max = 8388608
+net.ipv4.tcp_rmem = 4096 87380 8388608
+net.ipv4.tcp_wmem = 4096 16384 8388608
+net.ipv4.tcp_slow_start_after_idle = 0
