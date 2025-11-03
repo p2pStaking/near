387969658f27c205cd00cf5d3db34a6151d43415
p2pstaking <admin@p2pstaking.org>
2025-11-03 11:21:33 +0100

    fix init param

--- docker/initialize_near.sh
+++ docker/initialize_near.sh
@@ -2,12 +2,9 @@
 
 if [ ! -f  ~/.near/genesis.json ] ; then 
 	STAKING_POOL_ID=${POOL_ID}.poolv1.near
-	mkdir ~/.near
+	mkdir -p  ~/.near
 	cd ~/.near
-	/usr/local/bin/neard init --chain-id="$NEAR_ENV"  --account-id="$STAKING_POOL_ID" 
-	rm /root/.near/config.json /root/.near/genesis.json
-	wget -c https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/${NEAR_ENV}/config.json
-	wget -c https://s3-us-west-1.amazonaws.com/build.nearprotocol.com/nearcore-deploy/${NEAR_ENV}/genesis.json
+	/usr/local/bin/neard init --chain-id="$NEAR_ENV"  --account-id="$STAKING_POOL_ID" --download-config 
 	near generate-key ${POOL_ID}
 fi
 
